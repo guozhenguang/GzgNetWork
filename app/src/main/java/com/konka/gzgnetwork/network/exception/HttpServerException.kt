070e2e825f873d6284b3a63fd739141d2a7879e0
guozhenguang <guozhenguang@konka.com>
2022-11-08 09:34:44 +0800

    网络异常打印

--- app/src/main/java/com/konka/gzgnetwork/network/exception/HttpServerException.kt
+++ app/src/main/java/com/konka/gzgnetwork/network/exception/HttpServerException.kt
@@ -1,4 +1,10 @@
 package com.konka.gzgnetwork.network.exception
 
+import com.konka.gzgnetwork.ext.logd
+
 class HttpServerException(val code: String, message: String) : Exception(message) {
+    init {
+        val TAG = "HttpServerException"
+        "HttpServerException code: $code, message:$message".logd(TAG)
+    }
 }
